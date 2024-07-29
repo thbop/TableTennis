44bf28022711904f510e621a1aab665fd3442c6d
Thbop <75861321+thbop@users.noreply.github.com>
2024-07-29 14:07:44 -0400

    Added the player paddle, movement, and animation

--- TableTennis.c
+++ TableTennis.c
@@ -9,8 +9,14 @@
 //     return *(float*) &v;
 // }
 
+#define GETXY(p) (Vector2){ p.x, p.y }
+
 Vector3 Vector3MultiplyValue(Vector3 p, float v) {
     return (Vector3){ p.x * v, p.y * v, p.z * v };
+}
+
+Vector2 Vector2MultiplyValue(Vector2 p, float v) {
+    return (Vector2){ p.x * v, p.y * v };
 }
 
 // Typedefs
@@ -30,6 +36,13 @@ typedef unsigned char u8;
 
 #define BALLCOLOR (Color){ 235, 235, 235, 255 }
 
+#define PADDLETEXDIM     48.0f
+#define PADDLEMAXFRAME   9
+#define PADDLEFRAMEDELAY 1
+
+#define PLAYERACC 0.1;
+
+// Ball
 struct {
     Vector3 pos, vel;
     float floor_z, radius;
@@ -59,6 +72,9 @@ void ball_move() {
     if ( ball.pos.z <= ball.floor_z ) { ball.pos.z = ball.floor_z; ball.vel.z = -ball.vel.z; }
     else if ( ball.vel.z > -3.0f ) ball.vel.z -= 0.02;
 
+    // Ceiling
+    if ( ball.pos.z > 15.0f ) ball.pos.z = 15.0f;
+
     // Wall Bounce
     if ( fabsf(ball.pos.x) >= 45.0f )                  ball.vel.x = -ball.vel.x;
     if ( ball.pos.y <= 45.0f || ball.pos.y >= HEIGHT ) ball.vel.y = -ball.vel.y;
@@ -76,6 +92,68 @@ void ball_init() {
     ball.radius = 3.0f;
 }
 
+// Paddles
+typedef struct {
+    Texture2D tex;
+    int frame, frametick;
+    u8 animate;
+    Vector2 pos, vel;
+} paddle;
+
+paddle paddle_init( char* texFileName, Vector2 pos ) {
+    paddle p;
+    p.tex = LoadTexture(texFileName);
+    p.frame = p.frametick = p.animate = 0;
+    p.pos = pos;
+    p.vel = (Vector2){ 0.0f, 0.0f };
+    return p;
+}
+
+void paddle_animate(paddle* p) {
+    if ( p->animate ) {
+        if ( p->frametick < PADDLEFRAMEDELAY ) p->frametick++;
+        else {
+            p->frametick = 0;
+            if ( p->frame < PADDLEMAXFRAME ) p->frame++;
+            else { p->frame = 0; p->animate = 0; }
+        }
+    }
+    DrawTextureRec(
+        p->tex,
+        (Rectangle){ p->frame*PADDLETEXDIM, 0.0f, PADDLETEXDIM, PADDLETEXDIM },
+        p->pos, WHITE
+    );
+}
+
+void paddle_unload(paddle p) {
+    UnloadTexture(p.tex);
+}
+
+// Player
+paddle player;
+void player_init() {
+    player = paddle_init(
+        "graphics/player_paddle.png",
+        (Vector2){ 100.0f, 110.0f }
+    );
+}
+
+void player_move() {
+    if ( IsKeyDown(KEY_D) ) player.vel.x += PLAYERACC;
+    if ( IsKeyDown(KEY_A) ) player.vel.x -= PLAYERACC;
+
+    player.vel.x *= 0.98f;
+    player.pos = Vector2Add( player.pos, player.vel );
+
+    // printf("%f\n", player.pos.x);
+    if      ( player.pos.x > 155.0f ) { player.vel.x = -player.vel.x*0.5f; player.pos.x = 155.0f; }
+    else if ( player.pos.x < -15.0f ) { player.vel.x = -player.vel.x*0.5f; player.pos.x = -15.0f; }
+
+    if ( IsKeyPressed(KEY_SPACE) ) {
+        player.animate = 1;
+    }
+}
+
 
 RenderTexture2D screen;
 Texture2D background, table, table_mask;
@@ -87,7 +165,6 @@ int table_mask_loc;
 void Init() {
     InitWindow(WINWIDTH, WINHEIGHT, "Table Tennis!");
     SetTargetFPS(FPS);
-    ball_init();
 
     screen = LoadRenderTexture(WIDTH, HEIGHT);
     background = LoadTexture("graphics/background.png");
@@ -97,7 +174,8 @@ void Init() {
     shadow_shader = LoadShader(0, "shadow.glsl");
     table_mask_loc = GetShaderLocation(shadow_shader, "mask");
 
-    
+    ball_init();
+    player_init();
 
     // a = 0.0f;
 }
@@ -113,6 +191,7 @@ void Unload() {
 
 void Update() {
     ball_move();
+    player_move();
     // printf("%f\n", ball.pos.x);
 
     // if (IsKeyDown(KEY_RIGHT)) a+=0.5f;
@@ -138,6 +217,8 @@ void Draw() {
     // printf("%f\n", a.y);
 
     // DrawLineV( project((Vector3){a, 45.0f, 0.0f}), project((Vector3){a, WIDTH, 0.0f}), GREEN );
+
+    paddle_animate(&player);
 }
 
 
